115dd793eb5cafbfbf8be614592b72290d15ef3e
Nandhikar21 <gnandhikar@gmail.com>
2025-11-10 14:30:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Smooth scroll animations using ScrollReveal
 ScrollReveal().reveal('.hero-content', {
-  distance: '50px',
+  distance: '60px',
   duration: 1000,
   origin: 'bottom',
   reset: false,
 });
 
-ScrollReveal().reveal('.section', {
+ScrollReveal().reveal('.card, .research-card', {
   distance: '60px',
-  duration: 1200,
+  duration: 1000,
   origin: 'bottom',
-  interval: 200,
+  interval: 150,
   reset: false,
 });
